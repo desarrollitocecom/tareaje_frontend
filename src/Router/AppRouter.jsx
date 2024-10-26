9944104d56fc9d0c6cc0abe99b9c31242ba9697b
Barb Desktop <brunobarbaran@icloud.com>
2024-10-26 13:34:01 -0500

    Actualización en componentes para filtros, tablas y navegación

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -9,6 +9,7 @@ import SeguimientoAsistencia from "../Pages/SeguimientoAsitencia";
 import Login from '../Pages/Login';
 import Error403 from '../Pages/Error403';
 import PersonalBD from '../Pages/PersonalBD';
+import Roles from '../Pages/Roles/Roles';
 
 const AppRouter = () => {
   const router = createBrowserRouter([
@@ -50,6 +51,70 @@ const AppRouter = () => {
             }
           ]
         },
+        {
+          path: "/roles",
+          element: <PrivateRouter element={<Roles />} />,
+        },
+        // {
+        //   path: "/usuarios",
+        //   element: <PrivateRouter element={<Usuarios />} />,
+        // },
+        // {
+        //   path: "/empleado",
+        //   element: <PrivateRouter element={<Empleado />} />,
+        // },
+        // {
+        //   path: "/cargo",
+        //   element: <PrivateRouter element={<Cargo />} />,
+        // },
+        // {
+        //   path: "/sexo",
+        //   element: <PrivateRouter element={<Sexo />} />,
+        // },
+        // {
+        //   path: "/grado-estudio",
+        //   element: <PrivateRouter element={<GradoEstudio />} />,
+        // },
+        // {
+        //   path: "/subgerencia",
+        //   element: <PrivateRouter element={<Subgerencia />} />,
+        // },
+        // {
+        //   path: "/funcion",
+        //   element: <PrivateRouter element={<Funcion />} />,
+        // },
+        // {
+        //   path: "/turno",
+        //   element: <PrivateRouter element={<Turno />} />,
+        // },
+        // {
+        //   path: "/regimen-laboral",
+        //   element: <PrivateRouter element={<RegimenLaboral />} />,
+        // },
+        // {
+        //   path: "/vacaciones",
+        //   element: <PrivateRouter element={<Vacaciones />} />,
+        // },
+        // {
+        //   path: "/descansos",
+        //   element: <PrivateRouter element={<Descansos />} />,
+        // },
+        // {
+        //   path: "/feriados",
+        //   element: <PrivateRouter element={<Feriados />} />,
+        // },
+        // {
+        //   path: "/justificaciones",
+        //   element: <PrivateRouter element={<Justificaciones />} />,
+        // },
+        // {
+        //   path: "/jurisdiccion",
+        //   element: <PrivateRouter element={<Jurisdiccion />} />,
+        // },
+        // {
+        //   path: "/lugar-trabajo",
+        //   element: <PrivateRouter element={<LugarTrabajo />} />,
+        // },
         {
           path: "/error",
           element: <PrivateRouter element={<Error403 />} />,
