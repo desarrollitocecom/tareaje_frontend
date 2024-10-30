28ab388592d93d6e201bdbd36490576719c97d16
Antonio <Antonio_cortez999@hotmail.com>
2024-10-30 17:18:16 -0500

    Agregacion de CRUDTABLE en asistencia del personal y base de datos del personal, agregacion de filtro subgerencia y responsive de buscador

--- src/Pages/AsistenciaPersonal.jsx
+++ src/Pages/AsistenciaPersonal.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { Formik, Form } from "formik";
 import useData from "../Components/Hooks/UseDB";
+import axios from 'axios';
+import CRUDTable from '../Components/Table/CRUDTable';
 import { Link, useNavigate } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -11,36 +13,67 @@ import FiltroSelect from "../Components/Filtroselect/Filtro";
 import { TextField } from "@mui/material";
 
 const AsistenciaPersonal = () => {
-  const { data, cargos, turnos } = useData();
+  const { cargos, turnos, subgerencias } = useData();
+  const [loading, setLoading] = useState(false);
+  const [dataFormatted, setDataFormatted] = useState([]);
+  const navigate = useNavigate();
+  const handlerowClick = (row) => {
+    console.log("Row clicked:", row);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`/DataEjemplo.json`);
+      const formattedData = response.data.data.map((item) => ({
+        id: item.member,
+        nombres: item.nombres,
+        apellidos: item.apellidos,
+        cargo: item.cargo,
+        telefono: item.telefono,
+      }));
+      setDataFormatted(formattedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <div className="w-full bg-gray-100 px-6 py-8 h-full flex flex-col overflow-auto overflow-y-scroll">
+    <div className='h-full flex flex-col w-full bg-gray-100 p-4'>
       <header className="text-white bg-green-700 py-4 px-3 mb-6 w-full rounded-lg flex justify-center relative">
-        <Link onClick={() => navigate(-1)} className="flex items-center gap-1">
-          <ArrowBackIosNewRoundedIcon
-            className="!size-5 md:!size-6 mt-[0.1rem] absolute left-4"
-          />
-        </Link>
-        <h1 className="md:text-2xl lg:text-4xl font-bold text-center">
-          ASISTENCIA DEL PERSONAL
-        </h1>
-      </header>
+          <Link onClick={() => navigate(-1)} className='flex items-center gap-1'>
+            <ArrowBackIosNewRoundedIcon
+              className='!size-5 md:!size-6 mt-[0.1rem] absolute left-4'
+            />
+          </Link>
+          <h1 className="md:text-2xl lg:text-4xl font-bold text-center">
+            ASISTENCIA DEL PERSONAL
+          </h1>
+        </header>
+      <main className='flex-1 bg-white shadow rounded-lg p-4 h-full overflow-hidden'>
+        <div className='flex flex-col w-full h-full'>
 
       <Formik
         initialValues={{
           buscar: "",
-          fecha: null, // Iniciar con null para el DatePicker
+          fecha: null,
           cargo: "",
           turno: "",
+          subgerencias: ""
         }}
         onSubmit={(values) => {
-          // Aquí puedes manejar la lógica de envío del formulario
           console.log("Valores del formulario:", values);
         }}
       >
         {({ handleChange, handleBlur, values, setFieldValue, errors, touched }) => (
-          <Form className="flex flex-col flex-1 text-nowrap">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-4 text-sm">
+          <Form className="flex flex-col text-nowrap">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mb-4 text-sm">
               {/* Buscar */}
               <div className="md:col-span-1">
                 <TextField
@@ -63,19 +96,27 @@ const AsistenciaPersonal = () => {
                     label="Seleccionar Fecha"
                     value={values.fecha}
                     slotProps={{ textField: { size: 'small' } }}
-                    className="bg-white"
+                    maxDate={dayjs()}
+                    format="DD/MM/YYYY"
                     onChange={(value) => setFieldValue("fecha", value)}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        variant="outlined"
-                        className="bg-white"
-                        error={Boolean(errors.fecha && touched.fecha)}
-                        helperText={touched.fecha && errors.fecha}
-                      />
-                    )}
+                    
+                    
                   />
                 </LocalizationProvider>
+              </div>
+
+              {/* Seleccionar Subgerencia */}
+              <div>
+                <FiltroSelect
+                  label="Subgerencia"
+                  name="subgerencias"
+                  value={values.subgerencias}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  options={subgerencias}
+                  error={errors.subgerencias}
+                  touched={touched.subgerencias}
+                />
               </div>
 
               {/* Seleccionar Cargo */}
@@ -108,58 +149,20 @@ const AsistenciaPersonal = () => {
             </div>
 
             {/* Tabla de asistencia */}
-            <div className="overflow-x-auto text-sm text-nowrap">
-              <table className="min-w-full table-auto border-collapse bg-white shadow-md rounded-lg">
-                <thead className="bg-green-600 text-white">
-                  <tr>
-                    <th className="p-2">#</th>
-                    <th className="p-2">Nombre</th>
-                    <th className="p-2">Apellido</th>
-                    <th className="p-2">DNI</th>
-                    <th className="p-2">Puesto</th>
-                    <th className="p-2">Turno</th>
-                    <th className="p-2">Fecha y Hora</th>
-                    <th className="p-2">Imagen</th>
-                    <th className="p-2">Captura</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((item, index) => (
-                    <tr
-                      key={item.id}
-                      className={`border-t ${index % 2 === 0 ? "bg-white" : "bg-gray-100"}`}
-                    >
-                      <td className="p-2 text-center">{index + 1}</td>
-                      <td className="p-2 text-center">{item.nombre}</td>
-                      <td className="p-2 text-center">{item.apellido}</td>
-                      <td className="p-2 text-center">{item.dni}</td>
-                      <td className="p-2 text-center">{item.puesto}</td>
-                      <td className="p-2 text-center">{item.turno}</td>
-                      <td className="p-2 text-center">
-                        {item.fecha}, {item.hora}
-                      </td>
-                      <td className="p-2 text-center min-w-20">
-                        <img
-                          src={item.imagen}
-                          alt="Imagen de usuario"
-                          className="max-w-20 object-cover mx-auto rounded-lg aspect-square"
-                        />
-                      </td>
-                      <td className="p-2 text-center min-w-20">
-                        <img
-                          src={item.captura}
-                          alt="Imagen de usuario"
-                          className="max-w-20 object-cover mx-auto rounded-lg aspect-square"
-                        />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+          
+              
           </Form>
         )}
       </Formik>
+      <div className="my-1"></div>
+
+        <CRUDTable 
+        data={dataFormatted} 
+        loading={loading} 
+        rowOnClick={handlerowClick}
+        />
+        </div>
+      </main>
     </div>
   );
 };
