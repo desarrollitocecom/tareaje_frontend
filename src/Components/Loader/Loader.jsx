ceb103ad55ad20ec08afcb39067ea60425ec38c2
Barb Desktop <brunobarbaran@icloud.com>
2024-11-05 13:57:24 -0500

    Cambios de estilos

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/logos/logo_sjl.png'
 
 const Loader = () => {
     return (
-        <div id='loader' className='bg-white/75 z-[9999] fixed top-0 left-0 w-full h-full flex items-center justify-center'>
+        <div id='loader' className='bg-white/80 z-[9999] fixed top-0 left-0 w-full h-full flex items-center justify-center'>
             <div className='container flex items-center justify-center'>
                 <img className='max-w-[200px] animate-scaleUpDown drop-shadow-md' src={Logo} alt="logo_sjl" />
             </div>
