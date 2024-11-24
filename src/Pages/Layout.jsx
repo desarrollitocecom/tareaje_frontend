bcb4fe8618681f09f8fbed3a52384a2eb8c82dc1
Barb <brunobarbaran@icloud.com>
2024-11-23 23:19:13 -0500

    correcion de carga doble de peticiones

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -5,42 +5,47 @@ import Header from '../Components/Navigation/Header'
 import UseLogin from './Login/UseLogin'
 import { useDispatch, useSelector } from 'react-redux'
 import CustomSwal from '../helpers/swalConfig'
-import { loginSuccess, logout } from '../Redux/Slices/AuthSlice'
+import { loginSuccess, logout, setIsLogin } from '../Redux/Slices/AuthSlice'
 import { socket } from '../Components/Socket/socket'
 import Loader from '../Components/Loader/Loader'
 
 const Layout = () => {
     const [toggled, setToggled] = useState(false);
     const dispatch = useDispatch();
-    const { refresh, token, moduleLoading } = useSelector((state) => state.auth);
+    const { refresh, token, isLogin } = useSelector((state) => state.auth);
     const { getUserData } = UseLogin()
 
     useEffect(() => {
+        if (!isLogin) {
+            console.log('Se ejecuto el login');
+            
+            const fetchData = async () => {
+                try {
+                    const userData = await getUserData(token);
+                    dispatch(loginSuccess({ user: userData, token: token }));
 
-        const fetchData = async () => {
-            try {
-                const userData = await getUserData(token);
-                dispatch(loginSuccess({ user: userData, token: token }));
+                } catch (error) {
+                    console.error(error);
+                }
+            };
 
-            } catch (error) {
-                console.error(error);
-            }
-        };
+            fetchData()
 
-        fetchData()
+            socket.on('forceLogout', (data) => {
+                if (data) {
+                    CustomSwal.fire({
+                        icon: 'warning',
+                        title: 'Nueva sesión detectada',
+                        text: 'Revisa la seguridad de tu cuenta si no reconoces esta actividad.',
+                        didClose: () => {
+                            dispatch(logout());
+                        }
+                    });
+                }
+            })
+        }
 
-        socket.on('forceLogout', (data) => {
-            if (data) {
-                CustomSwal.fire({
-                    icon: 'warning',
-                    title: 'Nueva sesión detectada',
-                    text: 'Revisa la seguridad de tu cuenta si no reconoces esta actividad.',
-                    didClose: () => {
-                        dispatch(logout());
-                    }
-                });
-            }
-        })
+        dispatch(setIsLogin(false))
 
         return () => {
             socket.off('foreLogout');
@@ -52,7 +57,7 @@ const Layout = () => {
         <div className='flex h-full w-full'>
             <Sidebar toggled={toggled} setToggled={setToggled} />
             <div className='w-full overflow-auto flex flex-col relative'>
-                 <Loader />
+                <Loader />
                 <Header toggled={toggled} setToggled={setToggled} />
                 <div className='content-body bg-gray-100 flex flex-1 overflow-hidden'>
                     <Outlet />
